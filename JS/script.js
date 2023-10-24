16e8371c445a498d1db295995a86d2225794d96f
carlomarcon <carlomarcon1998@gmail.com>
2023-10-24 22:17:42 +0200

    ADD: JS SCROLL

--- JS/script.js
+++ JS/script.js
@@ -26,18 +26,65 @@ ImgDiv.innerHTML += Insert;
 document.getElementsByClassName("item")[0].classList.add("active");
 let index = 0;
 
-buttonUp.addEventListener("click", function () {
-  // SELEZIONARE IL DIV CON CLASSE ACTIVE
-
-  // TOGLIAMO LA CLASSE ACTIVE
-
-  // I++
-  // SELENEZIONO DALL'ARRAY ITEM(S) I esimo
-  // AGGIUNGO LA CLASSE ACTIVE
-
-  if (i == 0) {
-    CurrentImage.classList.add(".active");
+document.querySelector(".prev").addEventListener("click", function () {
+  index++;
+  if (index === 1) {
+    document.getElementsByClassName("item")[index].classList.add("active");
+    document
+      .getElementsByClassName("item")
+      [index - 1].classList.remove("active");
+  } else {
+    document.getElementsByClassName("item")[index].classList.add("active");
+    document
+      .getElementsByClassName("item")
+      [index - 1].classList.remove("active");
   }
+  console.log(index);
 });
+
+document.querySelector(".next").addEventListener("click", function () {
+  index--;
+  if (index === 1) {
+    document.getElementsByClassName("item")[index].classList.add("active");
+    document
+      .getElementsByClassName("item")
+      [index + 1].classList.remove("active");
+  } else {
+    document.getElementsByClassName("item")[index].classList.add("active");
+    document
+      .getElementsByClassName("item")
+      [index + 1].classList.remove("active");
+  }
+  console.log(index);
+});
+
+// document.querySelector(".prev").addEventListener("click", function () {
+//   for (let index = 1; index < ArrayImg.length; ++index) {
+//     if (index === 1) {
+//       document.getElementsByClassName("item")[index].classList.add("active");
+//       document
+//         .getElementsByClassName("item")
+//         [index - 1].classList.remove("active");
+//     } else if (index === 2) {
+//       document.getElementsByClassName("item")[index].classList.add("active");
+//       document
+//         .getElementsByClassName("item")
+//         [index - 1].classList.remove("active");
+//     }
+//     console.log(index);
+//   }
+// });
+
+// SELEZIONARE IL DIV CON CLASSE ACTIVE
+
+// TOGLIAMO LA CLASSE ACTIVE
+
+// I++
+// SELENEZIONO DALL'ARRAY ITEM(S) I esimo
+// AGGIUNGO LA CLASSE ACTIVE
+
+// if (i == 0) {
+//   CurrentImage.classList.add(".active");
+// }
 
 console.log(ArrayImg[1]);
